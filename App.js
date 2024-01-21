11ea8c63061e03d4e0fd3fc071382f25fbde1657
“Iuliia” <ybe2004ybe@gmail.com>
2024-01-21 20:35:43 +0200

    Complete

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Button, Text, StyleSheet } from 'react-native';
+import { Keyboard } from 'react-native';
 
-export default function App() {
+const CalculatorApp = () => {
+  const [number1, setNumber1] = useState('');
+  const [number2, setNumber2] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleSum = () => {
+    const sum = parseFloat(number1) + parseFloat(number2);
+    setResult(sum.toString());
+    Keyboard.dismiss();
+  };
+
+  const handleSubtraction = () => {
+    const subtraction = parseFloat(number1) - parseFloat(number2);
+    setResult(subtraction.toString());
+    Keyboard.dismiss();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.result}>Result: {result}</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter number 1"
+        keyboardType="numeric"
+        value={number1}
+        onChangeText={(text) => setNumber1(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter number 2"
+        keyboardType="numeric"
+        value={number2}
+        onChangeText={(text) => setNumber2(text)}
+      />
+      <View style={styles.buttonsContainer}>
+        <Button title="+" onPress={handleSum} />
+        <Button title="-" onPress={handleSubtraction} />
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  result: {
+    marginBottom: 20,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '40%',
+    marginBottom: 10,
   },
 });
+
+export default CalculatorApp;
